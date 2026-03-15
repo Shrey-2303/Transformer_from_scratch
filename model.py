e9ca7a57721adef244de5d3081024a208b0875a6
Shrey-2303 <shrey.shah2303@gmail.com>
2026-03-15 16:13:40 -0400

    layer rnormalization

--- model.py
+++ model.py
@@ -39,4 +39,16 @@ class PositionalEncding(nn.Module):
         return self.dropout(x)
     
     
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 1e-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.gamma = nn.parameter(torch.ones(1))
+        self.beta = nn.parameter(torch.zeros(1))
+        
+    def forward(self,x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+        
         
