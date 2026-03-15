f499fe594c67ff3274e4507579abf892448e1948
Shrey-2303 <shrey.shah2303@gmail.com>
2026-03-15 16:04:00 -0400

    embedding and positional enodings

--- model.py
+++ model.py
@@ -1,9 +1,42 @@
 # main file
 import torch 
 import torch.nn as nn
-
+import math
 
 class InputEmbeddings(nn.Modul):
-    def __init__(self, d_models: int, vocab_size: int):
+    def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self,x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+
+class PositionalEncding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # create matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len,d_model)
+        position = torch.arange(0,seq_len-1, dtype = torch.float).unsequeez(1)  # (seq_len,1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_model))
+        
+        # apply sin and cos positions
+        pe[:,0::2] = torch.sin(position*div_term)
+        pe[:,1::2] = torch.cos(position*div_term)
+        
+        pe = pe.unsqueeze(0) # add extra dim in the front for batches
+        
+        self.register_buffer('pe',pe) # register a tensor which is not a learnt parameter, like a variable being stored)
+        
+    def forward(self,x):
+        x = x + (self.pe[:,:x.shape[1],:]).requires_grad_(False)
+        return self.dropout(x)
+    
+    
         
